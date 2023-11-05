31b87f70e3802b69ffac528e9177ee6e6d4dc193
alharidt16 <al_haridt@mhs.unsyiah.ac.id>
2023-11-05 22:12:25 +0700

    a

--- TA.R
+++ TA.R
@@ -10,25 +10,31 @@ MYDATA=DATA_TA
 DATA_TA=DATA_TA[,c(-1,-2,-38)]
 str(DATA_TA)
 
-k_medoids=pam(DATA_TA, 5, metric = "manhattan", stand = FALSE)
+#k_medoids=pam(DATA_TA, 5, metric = "manhattan", stand = FALSE)
 
 k_medoids$clustering
 k_medoids$medoids
 
-"""
+
 #SAMPLE
 
-sampel2 <- read_excel("sampel2.xlsx", sheet = "Sheet2")
-k_medoids_sample=pam(sampel2, 5, metric = "manhattan", stand = FALSE)
-k_medoids_sample
+#sampel2 <- read_excel("sampel2.xlsx", sheet = "Sheet2")
+#k_medoids_sample=pam(sampel2, 5, metric = "manhattan", stand = FALSE)
+#k_medoids_sample
 
 
-MYDATA$cluster_kmedian<-k_medoids$clustering
+
+library(klaR)
+
+k_modes=kmodes(DATA_TA, 5, iter.max = 10, weighted = FALSE, fast = TRUE)
+k_modes$cluster
+
+MYDATA$cluster_kmedoids<-k_medoids$clustering
+MYDATA$cluster_kmodes<-k_modes$cluster
 View(MYDATA)
+
 
 
 #eksplor ke excel
 library(writexl)
-write_xlsx(MYDATA, path="DATA_kmedoids.xlsx")
-
-""""
+write_xlsx(MYDATA, path="MYDATA.xlsx")
